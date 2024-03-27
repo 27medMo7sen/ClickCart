d0ba20ab0f9c86e64267babaf747379ff27db107
27medMo7sen <am6945@fayoum.edu.eg>
2024-03-27 06:05:54 +0200

    Add listProducts function to product.controller.js

--- src/modules/Products/product.controller.js
+++ src/modules/Products/product.controller.js
@@ -6,17 +6,18 @@ import slugify from "slugify";
 import { customAlphabet } from "nanoid";
 import { pagination } from "../../utils/pagination.js";
 import cloudinary from "../../utils/coludinaryConfigrations.js";
+import match from "nodemon/lib/monitor/match.js";
 const nanoid = customAlphabet("123456_=!ascbhdtel", 5);
 
 export const createProduct = async (req, res) => {
   const { name, price, desc, stock, colors, sizes, appliedDiscount } = req.body;
   const { categoryId, subCategoryId, brandId } = req.query;
   const category = await categoryModel.findById(categoryId);
-  const subCategory = await subCategoryModel.findById(subCategoryId);
-  const brand = await brandModel.findById(brandId);
   if (!category) return next(new Error("Category not found", { cause: 400 }));
+  const subCategory = await subCategoryModel.findById(subCategoryId);
   if (!subCategory)
     return next(new Error("SubCategory not found", { cause: 400 }));
+  const brand = await brandModel.findById(brandId);
   if (!brand) return next(new Error("Brand not found", { cause: 400 }));
   const slug = slugify(name, "_");
   if (!req.files) return next(new Error("Images are required", { cause: 400 }));
@@ -34,6 +35,8 @@ export const createProduct = async (req, res) => {
     images.push({ secure_url, public_id });
     puplicIds.push(public_id);
   }
+  req.imagesPath = `${process.env.PROJECT_FOLDER}/category/${category.customId}/subCategory/${subCategory.customId}/brand/${brand.customId}/product/${customId}`;
+  req.customId = customId;
   const newProduct = await productModel.create({
     name,
     slug,
@@ -148,4 +151,42 @@ export const deleteProduct = async (req, res, next) => {
     `${process.env.PROJECT_FOLDER}/category/${category.customId}/subCategory/${subCategory.customId}/brand/${brand.customId}/product/${customId}`
   );
   res.status(200).json(deletedProduct);
+};
+export const listProducts = async (req, res, next) => {
+  //================= sort ===============
+  // const {sort}=req.query;
+  // const products = await productModel.find().sort(sort.replaceAll(","," "));
+  // if (!products) return next(new Error("Products not found", { cause: 404 }));
+  // res.status(200).json(products);
+  //================= select ===============
+  // const { select } = req.query;
+  // const products = await productModel
+  //   .find()
+  //   .select(select.replaceAll(",", " "));
+  // if (!products) return next(new Error("Products not found", { cause: 404 }));
+  // res.status(200).json(products);
+  //================= search ==============
+  // const { search } = req.query;
+  // const products = await productModel
+  //   .find({
+  //     $or: [
+  //       { name: { $regex: search, $options: "i" } },
+  //       { desc: { $regex: search, $options: "i" } },
+  //     ],
+  //   })
+  //======================= filter ========================
+  const excludeFields = ["page", "sort", "limit", "select", "search"];
+  excludeFields.forEach((key) => delete queryInstance[key]);
+  const queryInstance = { ...req.query };
+  console.log(queryInstance);
+  const queryString = JSON.parse(
+    JSON.stringify(queryInstance).replace(
+      /lte|gte|gt|lt|regex|in|nin|eq|neq/g,
+      (match) => `$${match}`
+    )
+  );
+  console.log(queryString);
+  const products = await productModel.find(queryString);
+  if (!products) return next(new Error("Products not found", { cause: 404 }));
+  res.status(200).json(products);
 };
