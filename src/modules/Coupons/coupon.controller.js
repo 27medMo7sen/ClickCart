ea4b1421a86ba2c58b5f4b9e81181da0831f494f
27medMo7sen <am6945@fayoum.edu.eg>
2024-03-27 03:04:07 +0200

    Update coupon controller to include createdAt and expireAt fields

--- src/modules/Coupons/coupon.controller.js
+++ src/modules/Coupons/coupon.controller.js
@@ -34,7 +34,7 @@ export const createCoupon = async (req, res, next) => {
 //============================================== updateCoupon ==============================================
 export const updateCoupon = async (req, res, next) => {
   const { couponId } = req.params;
-  const { code, discount } = req.body;
+  const { code, discount, createdAt, expireAt } = req.body;
   const found = await couponModel.findById({ _id: couponId });
   if (!found) return next(new Error("Coupon not found", { cause: 404 }));
   if (code) {
@@ -45,10 +45,48 @@ export const updateCoupon = async (req, res, next) => {
       return next(new Error("Coupon Code Already Exists", { cause: 400 }));
     found.code = code;
   }
-
   if (discount < 0 || discount > 100)
     return next(new Error("Invalid Discount", { cause: 400 }));
   found.discount = discount;
+  if (createdAt && expireAt) {
+    const createdAtMoment = moment(new Date(createdAt)).format(
+      "YYYY-MM-DD HH:mm:ss"
+    );
+    const expireAtMoment = moment(new Date(expireAt)).format(
+      "YYYY-MM-DD HH:mm:ss"
+    );
+    const now = moment().format("YYYY-MM-DD HH:mm:ss");
+    if (
+      moment(createdAtMoment).isSameOrAfter(moment(expireAtMoment)) ||
+      moment(now).isSameOrAfter(moment(expireAtMoment)) ||
+      moment(now).isAfter(moment(createdAtMoment))
+    )
+      return next(new Error("Invalid Dates", { cause: 400 }));
+    found.createdAt = createdAtMoment;
+    found.expireAt = expireAtMoment;
+  }
+  if (createdAtMoment) {
+    const createdAtMoment = moment(new Date(createdAt)).format(
+      "YYYY-MM-DD HH:mm:ss"
+    );
+    const now = moment().format("YYYY-MM-DD HH:mm:ss");
+    if (moment(now).isAfter(moment(createdAtMoment)))
+      return next(new Error("Invalid Dates", { cause: 400 }));
+    if (moment(createdAtMoment).isSameOrAfter(moment(found.expireAt)))
+      return next(new Error("Invalid Dates", { cause: 400 }));
+    found.createdAt = createdAtMoment;
+  }
+  if (expireAt) {
+    const expireAtMoment = moment(new Date(expireAt)).format(
+      "YYYY-MM-DD HH:mm:ss"
+    );
+    const now = moment().format("YYYY-MM-DD HH:mm:ss");
+    if (moment(now).isAfter(moment(expireAtMoment)))
+      return next(new Error("Invalid Dates", { cause: 400 }));
+    if (moment(expireAtMoment).isSameOrBefore(moment(found.createdAt)))
+      return next(new Error("Invalid Dates", { cause: 400 }));
+    found.expireAt = expireAtMoment;
+  }
   const updated = await found.save();
   if (!updated) return next(new Error("Coupon not updated", { cause: 500 }));
   res.status(200).json({ message: "Coupon updated succesfully", updated });
